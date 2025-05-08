223adea68181df54aac9b7cb38d58c2b96188606
DATA USER <data.user@DAFF.LOCAL>
2025-05-08 11:53:13 -0300

    Adicionando mais bancos a lista

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ bancos = [
     "Sicredi", "Banco Sicredi",
     "Banrisul", "Banco Banrisul",
     "BRB", "Banco BRB",
-    "PIX", "Pix", "Chave Pix", "Transferência Pix", "Pix Transferência", "C6 (336)"
+    "PIX", "Pix", "Chave Pix", "Transferência Pix", "Pix Transferência", "C6 (336)", "creditag"
 ]
 
 def tratar_dados(entrada):
